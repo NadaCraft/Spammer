b6d6911c8e3995bdeb37630cf9e5ed92e030348b
NadaCraft <120174290+NadaCraft@users.noreply.github.com>
2023-05-11 00:29:54 -0300

    Create Spammer.js

---
+++ Spammer.js
@@ -0,0 +1,29 @@
+var spam = [false,"Spam"];
+
+function procCmd(cmd){
+
+var cmd = cmd.split(" ");
+
+if(cmd[0]=="spam"){
+
+preventDefault();
+
+if(!cmd[1]){
+
+clientMessage("Use /spam <message>");
+
+}else if(cmd[1]=="off"){
+
+spam = [false,"Spam"];
+
+clientMessage("Spam disabled");
+
+}else{
+
+spam = [true,cmd.slice(1).join(" ")];}}}
+
+function modTick(){
+
+	if(spam[0]){
+
+Server.sendChat(spam[1]);}}
